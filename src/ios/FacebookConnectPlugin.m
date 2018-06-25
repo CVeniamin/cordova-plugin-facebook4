4668e0c5c6d483f5057ff45db5151b9efee6ea8f
Veniamin Craciun <veniamin.craciun@tecnico.ulisboa.pt>
2018-06-25 21:31:44 +0100

    Update FacebookConnectPlugin.m

--- src/ios/FacebookConnectPlugin.m
+++ src/ios/FacebookConnectPlugin.m
@@ -760,7 +760,6 @@ void FBMethodSwizzle(Class c, SEL originalSelector) {
 + (void)load
 {
     FBMethodSwizzle([self class], @selector(application:openURL:sourceApplication:annotation:));
-    FBMethodSwizzle([self class], @selector(application:openURL:sourceApplication:annotation:));
 }
 
 // This method is a duplicate of the other openURL method below, except using the newer iOS (9) API.
@@ -770,7 +769,7 @@ void FBMethodSwizzle(Class c, SEL originalSelector) {
     }
     // Required by FBSDKCoreKit for deep linking/to complete login
     [[FBSDKApplicationDelegate sharedInstance] application:application openURL:url sourceApplication:[options valueForKey:@"UIApplicationOpenURLOptionsSourceApplicationKey"] annotation:0x0];
-    
+
     // NOTE: Cordova will run a JavaScript method here named handleOpenURL. This functionality is deprecated
     // but will cause you to see JavaScript errors if you do not have window.handleOpenURL defined:
     // https://github.com/Wizcorp/phonegap-facebook-plugin/issues/703#issuecomment-63748816
@@ -793,12 +792,14 @@ void FBMethodSwizzle(Class c, SEL originalSelector) {
     // Required by FBSDKCoreKit for deep linking/to complete login
     [[FBSDKApplicationDelegate sharedInstance] application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
 
+    // Call existing method
+    [self swizzled_application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
+    
     // NOTE: Cordova will run a JavaScript method here named handleOpenURL. This functionality is deprecated
     // but will cause you to see JavaScript errors if you do not have window.handleOpenURL defined:
     // https://github.com/Wizcorp/phonegap-facebook-plugin/issues/703#issuecomment-63748816
     NSLog(@"FB handle url: %@", url);
     
-    // Call existing method
-    return [self swizzled_application:application openURL:url sourceApplication:sourceApplication annotation:annotation];
+    return YES;
 }
 @end
